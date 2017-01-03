0a74b49ef41ed0f8b01fb9c3735d12928f1a1e5f
BenF777 <ben.flies@lns.etat.lu>
2017-01-03 09:58:25 +0100

    added additional databases

--- scripts/dna/anovar_annotate.sh
+++ scripts/dna/anovar_annotate.sh
@@ -8,4 +8,4 @@ CSV_OUT=${FILENAME%.vcf}
 $ANNOVAR_HOME/convert2annovar.pl -format vcf4 $FILENAME -outfile $AVINPUT -include -withzyg -includeinfo
 #$ANNOVAR_HOME/table_annovar.pl $AVINPUT $ANNOVAR_DB -buildver hg19 -out $CSV_OUT -remove -otherinfo -protocol refGene,cytoBand,snp138,ljb_all,1000g2012apr_all,cosmic68 -operation g,r,f,f,f,f -nastring NA -csvout
 #$ANNOVAR_HOME/table_annovar.pl $AVINPUT $ANNOVAR_DB -buildver hg19 -out $CSV_OUT -remove -otherinfo -protocol refGene,cytoBand,snp138 -operation g,r,f -nastring NA -csvout
-$ANNOVAR_HOME/table_annovar.pl $AVINPUT $ANNOVAR_DB -buildver hg19 -out $CSV_OUT -remove -otherinfo -protocol refGene,cytoBand,snp138,ljb26_all -operation g,r,f,f -nastring NA -csvout
+$ANNOVAR_HOME/table_annovar.pl $AVINPUT $ANNOVAR_DB -buildver hg19 -out $CSV_OUT -remove -otherinfo -protocol refGene,cytoBand,genomicSuperDups,esp6500siv2_all,1000g2015auf_all,1000g2015_eur,exac03,avsnp147,dbnsfp30a,snp138,ljb26_all -operation g,r,r,f,f,f,f,f,f,f,f -nastring NA -csvout
