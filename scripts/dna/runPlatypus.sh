2883871b0f06e40b831cb2de325ca198317bb7a0
BenF777 <ben.flies@lns.etat.lu>
2017-01-03 13:14:46 +0100

    deleted unnecessary lines

--- scripts/dna/runPlatypus.sh
+++ scripts/dna/runPlatypus.sh
@@ -16,34 +16,17 @@ OUT=${OUT}_PLATYPUS
 REFERENCE=/home/benflies/NGS/references/hg19/ucsc.hg19.fa
 
 python $PLATYPUS_BINARY callVariants --bamFiles=$BAM_FILE --refFile=/home/benflies/NGS/references/hg19/ucsc.hg19.fa --output=$OUT.vcf --filterDuplicates=0 --nCPU=$DNA_PARALLEL_ALIGNMENT --genSNPs 0 --genIndels 1 --logFileName=$OUT.log
-#bcftools view ${OUT}.bcf > ${OUT}.vcf
 bgzip -c $OUT.vcf > $OUT.vcf.gz
 tabix -p vcf $OUT.vcf.gz
 
-#vcftools --vcf ${OUT}.vcf --recode --recode-INFO-all --bed $BED_FILE --out  ${OUT}_comp
-#mv ${OUT}_comp.recode.vcf  ${OUT}_comp.vcf
-
-#vcftools --vcf  ${OUT}_comp.vcf --remove-indels --recode --recode-INFO-all --out ${OUT}_comp_SNP
-#bgzip -c ${OUT}_comp_SNP.recode.vcf > ${OUT}_comp_SNP.recode.vcf.gz
-#tabix ${OUT}_comp_SNP.recode.vcf.gz
-
-#vcftools --vcf  ${OUT}_comp.vcf --keep-only-indels --recode --recode-INFO-all --out ${OUT}_comp_INDEL
-#bgzip -c ${OUT}_comp_INDEL.recode.vcf > ${OUT}_comp_INDEL.recode.vcf.gz
-#tabix ${OUT}_comp_INDEL.recode.vcf.gz
-
-#echo "FILTER Platypus"
-#Rscript $SCRIPT_PATH/dna/VariantFilter_platypus_SNP.R 40 0.1 0.1 ${OUT}_comp_SNP.recode.vcf.gz
-#Rscript $SCRIPT_PATH/dna/VariantFilter_platypus_INDEL.R 40 -1 -1 ${OUT}_comp_INDEL.recode.vcf.gz
+echo "FILTER Platypus"
 Rscript $SCRIPT_PATH/dna/VariantFilter_platypus_INDEL.R 40 -1 -1 ${OUT}.vcf.gz
 
-
 #variant annotation
-#echo "VARIANT ANNOTATION"
-#bash $SCRIPT_PATH/dna/anovar_annotate.sh $CONFIG_FILE $(ls -d -1 ${OUT}_comp_SNP.recode_filtered*.vcf)
-#bash $SCRIPT_PATH/dna/anovar_annotate.sh $CONFIG_FILE $(ls -d -1 ${OUT}_comp_INDEL.recode_filtered*.vcf)
-bash $SCRIPT_PATH/dna/anovar_annotate.sh $CONFIG_FILE $(ls -d -1 ${OUT}_filtered*.vcf)
+echo "VARIANT ANNOTATION"
 
-#Rscript $SCRIPT_PATH/dna/annovar_csv2xlsx_platypus.R  $(ls -d -1 ${OUT}_comp_SNP.recode_filtered*.hg19_multianno.csv)
-#Rscript $SCRIPT_PATH/dna/annovar_csv2xlsx_platypus.R  $(ls -d -1 ${OUT}_comp_INDEL.recode_filtered*.hg19_multianno.csv)
+bash $SCRIPT_PATH/dna/anovar_annotate.sh $CONFIG_FILE $(ls -d -1 ${OUT}_filtered_DP40-1.vcf)
 
-#echo "$OUT finished"
+Rscript $SCRIPT_PATH/dna/annovar_csv2xlsx_platypus.R  $(ls -d -1 ${OUT}_filtered_DP40-1.hg19_multianno.csv)
+
+echo "$OUT finished"
