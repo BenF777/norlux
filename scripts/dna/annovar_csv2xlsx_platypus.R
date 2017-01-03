9638a8e364b8aa4a715d25cc1b6105ee256aaad6
BenF777 <ben.flies@lns.etat.lu>
2017-01-03 13:13:05 +0100

    cancelled variant allele frequency calculation

--- scripts/dna/annovar_csv2xlsx_platypus.R
+++ scripts/dna/annovar_csv2xlsx_platypus.R
@@ -5,17 +5,14 @@ file <- args[1]
 
 require(openxlsx)
 
-library(GenomicFeatures)
-library(ChIPpeakAnno)
-#library(BSgenome.Hsapiens.ensemble.grch37)
-
 csv <- read.csv(file, stringsAsFactors=FALSE)
 
 if(nrow(csv) != 0) {
-
 otherInfo <- csv$Otherinfo
 
 info <- strsplit(otherInfo, ";")
+print(info)
+
 dp.col <- grepl("TC=", info[[1]])
 nf.col <- grepl("NF=", info[[1]])
 nr.col <- grepl("NR=", info[[1]])
@@ -33,27 +30,27 @@ sample <- gsub("nan,nan,nan", ".", sample)
 
 out <- csv
 
-af_list <- c()
+#af_list <- c()
 
-for (i in dp4) {
-  ref.for <- as.numeric(unlist(strsplit(i,split=","))[1])
-  ref.rev <- as.numeric(unlist(strsplit(i,split=","))[2])
-  alt.for <- as.numeric(unlist(strsplit(i,split=","))[3])
-  alt.rev <- as.numeric(unlist(strsplit(i,split=","))[4])
-  if (ref.for != 0 & ref.rev != 0) {
-    ratioRef.for <- ref.for/(ref.for+ref.rev)
-    ratioRef.rev <- ref.for/(ref.for+ref.rev)
-    ratioAlt.for <- alt.for/(alt.for+alt.rev)
-    ratioAlt.rev <- alt.rev/(alt.for+alt.rev)
-    af <- (alt.for + alt.rev) /(ref.for+ref.rev+alt.for + alt.rev)
-  }
-  else {
-  af <- 1
-  }
-  af_list<-c(af_list,af)
-}
+#for (i in dp4) {
+#  ref.for <- as.numeric(unlist(strsplit(i,split=","))[1])
+#  ref.rev <- as.numeric(unlist(strsplit(i,split=","))[2])
+#  alt.for <- as.numeric(unlist(strsplit(i,split=","))[3])
+#  alt.rev <- as.numeric(unlist(strsplit(i,split=","))[4])
+#  if (ref.for != 0 & ref.rev != 0) {
+#    ratioRef.for <- ref.for/(ref.for+ref.rev)
+#    ratioRef.rev <- ref.for/(ref.for+ref.rev)
+#    ratioAlt.for <- alt.for/(alt.for+alt.rev)
+#    ratioAlt.rev <- alt.rev/(alt.for+alt.rev)
+#    af <- (alt.for + alt.rev) /(ref.for+ref.rev+alt.for + alt.rev)
+#  }
+#  else {
+#  af <- 1
+#  }
+#  af_list<-c(af_list,af)
+#}
 
-out$VAF <- af_list
+#out$VAF <- af_list
 
 out$DP <- dp
 out$DP4 <- dp4
@@ -67,16 +64,17 @@ out$sample <- sample
 out <- out[,!(names(out) %in% c("Otherinfo"))]
 out[is.na(out)] <- "NA"
 
-out <- out[,c(1:11,23:33,12:22)]
+#out <- out[,c(1:11,23:33,12:22)]
 
     out.seq <- out
-} else
-{
-    cols <-  c("Chr", "Start", "End", "Ref", "Alt", "Func.refGene", "Gene.refGene", "ExonicFunc.refGene", "AAChange.refGene", "cytoBand", "snp138", "LJB_PhyloP", "LJB_PhyloP_Pred", "LJB_SIFT", "LJB_SIFT_Pred", "LJB_PolyPhen2", "LJB_PolyPhen2_Pred", "LJB_LRT", "LJB_LRT_Pred", "LJB_MutationTaster", "LJB_MutationTaster_Pred", "LJB_GERP..", "X1000g2012apr_all", "cosmic68", "Otherinfo")
-    out.seq <- as.data.frame(t(cols))[-1,]
-    colnames(out.seq) <- cols
-
 }
+#} else
+#{
+#    cols <-  c("Chr", "Start", "End", "Ref", "Alt", "Func.refGene", "Gene.refGene", "ExonicFunc.refGene", "AAChange.refGene", "cytoBand", "snp138", "LJB_PhyloP", "LJB_PhyloP_Pred", "LJB_SIFT", "LJB_SIFT_Pred", "LJB_PolyPhen2", "LJB_PolyPhen2_Pred", "LJB_LRT", "LJB_LRT_Pred", "LJB_MutationTaster", "LJB_MutationTaster_Pred", "LJB_GERP..", "X1000g2012apr_all", "cosmic68", "Otherinfo")
+#    out.seq <- as.data.frame(t(cols))[-1,]
+#    colnames(out.seq) <- cols
+#
+#}
 
 outFile <- paste(substr(file, 1, nchar(file)-3), "xlsx",sep="")
 outFileCsv <- paste(substr(file, 1, nchar(file)-3), "_sort.csv",sep="")
